41f63d80a77fbca04ba417bbfd1d86e97f32e952
HaithemGhattass <haithem.ghattas@covectors.io>
2023-07-14 21:17:14 +0100

    updates ..

---
+++ src/components/Optin/Optin.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./Optin.css";
+
+class Optin extends Component {
+  modal() {
+    const modal = document.getElementById("modal");
+    modal.classList.toggle("is_open");
+  }
+
+  render() {
+    return (
+      <div className="optin">
+        <p>zebi zebi ?</p>
+      
+      </div>
+    );
+  }
+}
+
+export default Optin;
